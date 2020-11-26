e75bc3f33c4045c2fcc0adb7c73bd5e6023bb0c7
shabbir12hasan <shabbir12hasan@gmail.com>
2020-11-26 15:17:12 +1100

    connector for mongoDB

--- src/stringdetector/libs/physical_data_model/connector.py
+++ src/stringdetector/libs/physical_data_model/connector.py
@@ -1,19 +1,29 @@
 import json
 import boto3
+from pymongo import MongoClient
 
 class DataConnector():
     def __init__(self):
-        with open("credentials.json", 'rb') as f:
+        with open("src/stringdetector/apps/detector_module/credentials.json", 'rb') as f:
+        # with open("credentials.json", 'rb') as f:
             self.creds = json.load(f)
-            
-        self.create_s3_connection()
 
-        
-        
-    
+        self.create_s3_connection()
+        self.create_mongo_connection()
+        # MongoDB database
+        self.flask_api_db = None
+
 
     def create_s3_connection(self):
         self.s3 = boto3.client('s3', aws_access_key_id=self.creds['aws']['key'],
          aws_secret_access_key= self.creds['aws']['secret'])
         self.s3_res = boto3.resource('s3', aws_access_key_id=self.creds['aws']['key'],
          aws_secret_access_key= self.creds['aws']['secret'])
+
+
+    def create_mongo_connection(self):
+        self.con_mongo = MongoClient(
+            self.creds['mongo']['server'],
+            username=self.creds['mongo']['user'],
+            password=self.creds['mongo']['pass']
+        )
